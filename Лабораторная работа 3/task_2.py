f1db6c8bc2438c6153cfe15e73906bb51cc2706c
korsakova-ei <korsakovae9@gmail.com>
2024-10-30 11:51:38 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_2.py
@@ -0,0 +1,26 @@
+# TODO Напишите функцию find_common_participants
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+
+def find_common_participants(group1, group2, separator=','):
+    # Разделяем строки на списки участников
+    participants1 = set(group1.split(separator))
+    participants2 = set(group2.split(separator))
+
+    # Находим общих участников
+    common_participants = participants1.intersection(participants2)
+
+    # Возвращаем отсортированный список общих участников
+    return sorted(common_participants)
+
+
+# Пример использования
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+# Проверка с разделителем '|'
+common_participants = find_common_participants(participants_first_group, participants_second_group, '|')
+print(common_participants)  # Вывод: ['Петров', 'Сидоров']
+# TODO Провеьте работу функции с разделителем отличным от запятой
